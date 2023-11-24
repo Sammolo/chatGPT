b33168a303c14a152319e81d64c8cd384ebbdc85
Sammolo <76825296+Sammolo@users.noreply.github.com>
2023-11-24 23:10:03 +0100

    Test per visibilità Key

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Guida per utilizzare il codice
+
+1. installare il pacchetto di OpenAI <br>
+<code> pip install --upgrade openai </code>
+
+2. generare e scaricare dal sito di [openAI](https://platform.openai.com/api-keys) la chiave ed inserirla tra le virgolette nella riga di codice<br>
+<code>key="inserisci qui la chiave che hai generato"</code>
+
+3. avviare il codice
--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("prova per vedere se sono sincronizzato con GIT")
+key=""
+
+from openai import OpenAI #importo libreria OpenAI
+
+
+client = OpenAI(api_key=key)
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
