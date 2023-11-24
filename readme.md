d85b395172a8c251628d961078794a43c5031f48
Sammolo <76825296+Sammolo@users.noreply.github.com>
2023-11-24 23:53:05 +0100

    Prima versione, non funzionante, assistente file

--- readme.md
+++ readme.md
@@ -6,4 +6,9 @@
 2. generare e scaricare dal sito di [openAI](https://platform.openai.com/api-keys) la chiave ed inserirla tra le virgolette nella riga di codice<br>
 <code>key="inserisci qui la chiave che hai generato"</code>
 
-3. avviare il codice
+3. inserire il percorso del file alla riga di codice **32** <br> 
+NB: [questi](https://platform.openai.com/docs/assistants/tools/supported-files) sono i file supportati al momento (colonna di destra) <br>
+<code>file = client.files.create( <br>
+  file=open("INSERISCI QUI IL TUO FILE.pdf", "rb"), <br>
+  purpose='assistants' <br>
+)</code>
--- test.py
+++ test.py
@@ -2,15 +2,52 @@ key=""
 
 from openai import OpenAI #importo libreria OpenAI
 
-
 client = OpenAI(api_key=key)
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
+#L'assistente si divide in 4 parti
+#1- creo l'assistente
+#NB: l'assistente prende 4 parametri che saranno fondamentali per la risposta che darà
+assistant = client.beta.assistants.create(
+    name="Primo assistente di test", #ignorabile
+    instructions="You are a data analyst. From a file draw a graphic", #qui specifico come l'assistente dovrebbe comportarsi o rispondere
+    model="gpt-4-32k-0314", #specifico quale gpt usare
+    tools=[{"type": "retrieval"}] #modalità per prendere file come input
 )
 
-print(completion.choices[0].message)
+
+#2- creo il tread quando l'utente inizia la conversazione
+thread = client.beta.threads.create()
+
+#3- aggiungo la domanda dell'utente come messaggio al tread
+message = client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content=input("Scrivi la domanda che vuoi fare all'assistente riguardo il file che hai caricato /n")
+)
+
+
+#4- faccio eseguire l'assistente sul tread per avere la risposta alla domanda
+#NB: per questo specifico assistente creo anche il codice per inserire il file che poi andrà ad analizzare
+file = client.files.create(
+  file=open("fileProva.pdf", "rb"),
+  purpose='assistants'
+)
+
+run = client.beta.threads.runs.create( #assegno l'assistente al tread
+  thread_id=thread.id,
+  assistant_id=assistant.id
+)
+
+#eseguo
+run = client.beta.threads.runs.retrieve(
+  thread_id=thread.id,
+  run_id=run.id
+)
+
+messages = client.beta.threads.messages.list(
+  thread_id=thread.id
+)
+
+
+
+#print(completion.choices[0].message)
